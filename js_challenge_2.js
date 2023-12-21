b914d72ec3fc7436cc42ef2f69aab2ff10d8d48b
ekksdee1 <112628685+ekksdee1@users.noreply.github.com>
2023-12-21 13:54:13 -0500

    finished challenge

--- js_challenge_2.js
+++ js_challenge_2.js
@@ -1,1 +1,10 @@
-
+var obj = document.body.getElementsByClassName("state");
+obj[0].addEventListener("click", function(){
+    var ans = prompt("In what state do you live?");
+    if(ans == ""){
+        alert("Where do you live?");
+    }
+    else {
+        alert("You live in " + ans + "!");
+    }
+});
